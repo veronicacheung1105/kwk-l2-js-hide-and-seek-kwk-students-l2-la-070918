5aead29f1c86a8c29bec21c3f616b49dadb29178
veronicacheung1105 <veronica.cheung1105@gmail.com>
2018-07-13 17:45:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //Code your solution here
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function increaseRankBy(n){
+  const unrankeds = document.getElementById('app').querySelectorAll('.ranked-list');
+  for (let i = 0; i < unrankeds.length; i++){ 
+    unrankeds[i].innerHTML = (unrankeds[i] + n).toString();
+  }
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div');
+}
